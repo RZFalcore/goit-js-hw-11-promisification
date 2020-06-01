432e59fe32994e96f4d744d0a6b8e312bfb2ab7e
RZFalcore <box.rz.box@gmail.com>
2020-06-01 19:14:10 +0300

    tasks complited

---
+++ Task2.js
@@ -0,0 +1,24 @@
+// Задание 2
+// Перепиши функцию toggleUserState() так, чтобы она не использовала callback - функцию callback,
+// а принимала всего два параметра allUsers и userName и возвращала промис.
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((res, rej) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    res(updatedUsers);
+  });
+};
+
+const logger2 = (updatedUsers) => console.table(updatedUsers);
+
+toggleUserState(users, "Mango").then(logger2);
+toggleUserState(users, "Lux").then(logger2);
